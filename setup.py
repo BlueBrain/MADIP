281704b3a1614c5e2491a795b4f27631789d418b
Polina Shichkova <polina.shichkova@epfl.ch>
2021-05-09 14:50:19 +0200

    add ACKNOWLEDGMENTS.md AUTHORS.txt CONTRIBUTING.md setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+#Blue Brain Project, EPFL
+
+"""
+MADIP: Molecular Atlas Data Integration Pipeline
+
+Copyright 2021 Blue Brain Project / EPFL 
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+
+"""
+
+pip install jupyterlab
+pip install seaborn
+pip install altair
+pip install scikit_posthocs
+
+pip install xlrd==1.2.0 # but only use for trusted xlsx files, see the xlrd security notes
+pip install networkx
+
